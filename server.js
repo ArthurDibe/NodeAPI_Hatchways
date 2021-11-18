28ec77e061aac6991c887c823a7ebe6b7cbd2818
Arthur Dibe <dibearthur@gmail.com>
2021-11-18 17:34:27 -0500

    Created basic server to run in PORT 3000

---
+++ server.js
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////
+//
+//   File: server.js
+//   Developer: Arthur Dibe
+// 
+//   HOW TO START:
+//     1) npm install
+//     2) npm start
+//     
+//   OBS: After starting the server, tests results outputs
+//        are saved in a file called "testResults.txt"
+//
+////////////////////////////////////////////////////////////
+
+const express = require("express")
+const fetch = require('node-fetch');
+const testAPI = require('./testSever.js')
+const app = express()
+// to parse the incoming requests
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) 
+// ------------------------------
+
+const HTTP_PORT = process.env.PORT || 3000
+
+app.listen(HTTP_PORT,()=>{
+    console.log(`[SERVER ON]: ${HTTP_PORT}`);
+    console.log("--------------------------------------------")
+})
+
