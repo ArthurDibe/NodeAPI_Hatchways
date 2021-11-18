dcd35586b17e5dceebdffb16f890f844b62a70bc
Arthur Dibe <dibearthur@gmail.com>
2021-11-18 17:36:57 -0500

    Created ROUTE 2 "/api/posts"

--- server.js
+++ server.js
@@ -34,3 +34,98 @@ app.get("/api/ping",(req, res)=>{
     res.status(200).json({success: true})
 })
 
+// ROUTE 2
+app.get("/api/posts", async (req, res)=> 
+{
+    
+    const tags = req.query.tags
+    const sortBy = req.query.sortBy || "id"
+    const direction = req.query.direction || "asc"
+    const sortOptions = ["id", "reads", "likes", "popularity"]
+    const directOptions = ["asc", "desc"]
+    let codeStatus = 400
+    let respObj
+    let responseAPI
+
+    console.log(`[SERVER ROUTE CALLED]: /api/posts?tags=${tags}&sortBy=${sortBy}&direction=${direction}`)
+    if(!tags){
+        respObj = {error: "Tags parameter is required"}
+    }
+    else if(!sortOptions.find(option => option == sortBy)){
+        respObj = {error: "sortBy parameter is invalid"}
+    }
+    else if(!directOptions.find(option => option == direction))
+    {
+        respObj = {error: "direction parameter is invalid"}
+    } 
+    else {
+        let responses = [];
+        const tagList = tags.split(',')
+        // ------------- Fetch the posts from API - synchronize by using Promise.all and .map
+        await Promise.all(tagList.map(async (tag) => 
+            {
+                const url = `https://api.hatchways.io/assessment/blog/posts?tag=${tag}`
+                const options = {
+                    "method": "GET",
+                }
+                
+                responseAPI = await fetch(url, options)
+                .then(resp1 => resp1.json())
+                
+                responses.push(...responseAPI.posts)
+            })
+        )
+        
+        // ------------- Sort the Elements according sort by
+        switch(sortOptions.findIndex(option=> option == sortBy)){
+        case 0:
+            // console.log("By ID")
+            if(direction == "asc") 
+                responses.sort((postA,postB) => postA.id - postB.id) // ascending
+            else 
+                responses.sort((postA,postB) => postB.id - postA.id) // descending
+            
+            break;
+        case 1:
+            // console.log("By READS")
+            if(direction == "asc") 
+                responses.sort((postA,postB) => postA.reads - postB.reads) // ascending
+            else
+                responses.sort((postA,postB) => postB.reads - postA.reads) // descending
+
+            break;
+        case 2:
+            // console.log("By LIKES")
+            if(direction == "asc") 
+                responses.sort((postA,postB) => postA.likes - postB.likes) // ascending
+            else
+                responses.sort((postA,postB) => postB.likes - postA.likes) // descending
+            
+            break;
+        case 3:
+            // console.log("By POPULARITY")
+            if(direction == "asc") 
+                responses.sort((postA,postB) => postA.popularity - postB.popularity) // ascending
+            else
+                responses.sort((postA,postB) => postB.popularity - postA.popularity) // descending
+
+            break;          
+        }
+
+        // ------------- Remove Repeated Objects
+        // responses.sort((postA,postB) => postA.id - postB.id)
+        var uniqueResponse = responses.reduce((first, second) => {
+            if(!first.some(obj => obj.id === second.id)) {
+              first.push(second);
+            }
+            return first;
+        },[]);
+        
+       // set the response object
+        codeStatus = 200
+        respObj = {posts: uniqueResponse}
+    }
+
+    // response
+    res.status(codeStatus).json(respObj)
+})
