831b1c302e7130d06484b77fa47123fd5f0fb9a5
Arthur Dibe <dibearthur@gmail.com>
2021-11-18 17:41:50 -0500

    Created testing routes that call "testRoute" function

--- testSever.js
+++ testSever.js
@@ -36,4 +36,44 @@ const testRoute = async (testNumber, url)=>
 }
 
 
+// Function called from server.js
+module.exports = testAPI = async()=> 
+{
+    // RESET the file to start over
+    fs.writeFile('testResults.txt', "",(err)=> {
+        if(err) throw err;
+        log("file reset")
+    })
 
+    // -----------> TEST 1 - 404 route not found 
+    testRoute('1.1','http://localhost:3000/')
+    testRoute('1.2','http://localhost:3000/postings')
+    testRoute('1.3','http://localhost:3000/posts')
+    testRoute('1.4','http://localhost:3000/api')
+    testRoute('1.5','http://localhost:3000/posts/api')
+
+    // TEST 2 - PING
+    testRoute('2','http://localhost:3000/api/ping')
+
+    // TEST 3 - /api/posts
+    const tagsExist = "politics,tech,health,history,startups,science,design,culture"
+    const tagsNotExist = "book,study,duty,paint,tv,warp,way,food"
+    const sortOptions = ["id", "reads", "likes", "popularity"]
+    const directOptions = ["asc", "desc"]
+
+    // -----------> TEST 3.1 - no TAGS provided
+    testRoute('3.1.1',`http://localhost:3000/api/posts`)
+    testRoute('3.1.2',`http://localhost:3000/api/posts?tags`)
+    sortOptions.forEach(option => testRoute('3.1.3',`http://localhost:3000/api/posts?tags&sortBy=${option}`))
+    directOptions.forEach(option => testRoute('3.1.4',`http://localhost:3000/api/posts?tags&direction=${option}`))
+
+    // -----------> TEST 3.2 - TAGS NOT EXIST
+    testRoute('3.2.1',`http://localhost:3000/api/posts?tags=${tagsNotExist}`)
+    sortOptions.forEach(option => testRoute('3.2.2',`http://localhost:3000/api/posts?tags=${tagsNotExist}&sortBy=${option}`))
+    directOptions.forEach(option => testRoute('3.2.3',`http://localhost:3000/api/posts?tags=${tagsNotExist}&direction=${option}`))
+
+    // -----------> TEST 3.3 - TAGS EXIST
+    testRoute('3.3.1',`http://localhost:3000/api/posts?tags=${tagsExist}`)
+    sortOptions.forEach(sortOpt => testRoute('3.3.2',`http://localhost:3000/api/posts?tags=${tagsExist}&sortBy=${sortOpt}&direction=asc`))
+    sortOptions.forEach(sortOpt => testRoute('3.3.3',`http://localhost:3000/api/posts?tags=${tagsExist}&sortBy=${sortOpt}&direction=desc`))
+}
