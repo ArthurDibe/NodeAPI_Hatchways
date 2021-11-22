ef9a788947feee24d3ba8d9f73254a7b14e81474
Arthur Dibe <dibearthur@gmail.com>
2021-11-21 23:39:06 -0500

    Created helper functions file and sortElements function

---
+++ helperFunctions.js
@@ -0,0 +1,37 @@
+const sortElements = (responses, sortOptions, sortBy, direction) => 
+{
+  switch (sortOptions.findIndex((option) => option == sortBy)) 
+  {
+    case 0:
+      if (direction == "asc")
+        responses.sort((postA, postB) => postA.id - postB.id);
+      else responses.sort((postA, postB) => postB.id - postA.id);
+
+      break;
+    case 1:
+      if (direction == "asc")
+        responses.sort((postA, postB) => postA.reads - postB.reads);
+      else responses.sort((postA, postB) => postB.reads - postA.reads);
+
+      break;
+    case 2:
+      if (direction == "asc")
+        responses.sort((postA, postB) => postA.likes - postB.likes);
+      else responses.sort((postA, postB) => postB.likes - postA.likes);
+
+      break;
+    case 3:
+      if (direction == "asc")
+        responses.sort((postA, postB) => postA.popularity - postB.popularity);
+      else
+        responses.sort((postA, postB) => postB.popularity - postA.popularity);
+
+      break;
+  }
+  
+  return responses
+};
+
+module.exports = {
+  sortElements: sortElements,
+};
